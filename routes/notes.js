293fa8bea8b458718c8a0ce648b470f4ede77804
Thomas James <tomsterlingjames@outlook.com.au>
2022-04-22 11:11:38 +1000

    GET route created in notes, tested in Insomnia and working

--- routes/notes.js
+++ routes/notes.js
@@ -6,3 +6,19 @@ const {
   writeToFile,
 } = require('../helpers/fsUtils');
 
+myNotes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+myNotes.get('/:note_id', (req, res) => {
+  const idNote = req.params.note_id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.note_id === idNote);
+      return result.length > 0
+        ? res.json(result)
+        : res.json('Sorry, there is no note with this ID');
+    });
+});
+
+module.exports = myNotes;
