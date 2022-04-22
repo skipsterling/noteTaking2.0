a101b763377c28525b7b9fa24bba574a3b8f559a
Thomas James <tomsterlingjames@outlook.com.au>
2022-04-22 10:08:22 +1000

    clog, express and links to routes added to server.js

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 const express = require('express');
-const PORT = process.env.PORT || 3001;
+const path = require('path');
+const { clog } = require('./middleware/clog');
+const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(clog);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}/`)
+);
