468500a1e1b4cafb49bb7073d442f1d699852e43
Thomas James <tomsterlingjames@outlook.com.au>
2022-04-22 10:10:34 +1000

    api added to server.js file

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
 const { clog } = require('./middleware/clog');
+const api = require('./routes/index');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(clog);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use('/api', api);
 
 app.use(express.static('public'));
 
